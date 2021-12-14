e2fa7d97ed6db3a17650cfd555f132b6453557fb
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-14 18:33:04 +0530

    Added Program

---
+++ LinqProject/LinqProject/Management.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+namespace LinqProject
+{
+    public class Management
+    {
+        public void TopRecordsList(List<ProductReview> review)
+        {
+            var recordData = (from productReviews in review
+                              orderby productReviews.Rating descending
+                              select productReviews).Take(3);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " " + "UserId: " + list.UserID + " " + "Rating: " + list.Rating + " "
+                    + "Review: " + list.Review + " " + list.isLike + " ");
+            }
+        }
+
+        public void SelectedRecords(List<ProductReview> review)
+        {
+            var recordData = from productReviews in review
+                             where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9)
+                             && productReviews.Rating > 3
+                             select productReviews;
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " " + "UserId: " + list.UserID + " " + "Rating: " + list.Rating + " "
+                    + "Review: " + list.Review + " " + list.isLike + " ");
+            }
+        }
+
+        public void RetrieveCountOfRecords(List<ProductReview> review)
+        {
+            var recordData = review.GroupBy(x => x.ProductID).
+                                    Select(x => new { ProductID = x.Key, Count = x.Count() });
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + "-------------" + list.Count);
+            }
+
+
+
+        }
+    }
+}
---
+++ LinqProject/LinqProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Revirew");
+            List<ProductReview> ListProductReview = new List<ProductReview>()
+            {
+                new ProductReview() {ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview() { ProductID = 2, UserID = 1, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 3, UserID = 2, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 4, UserID = 2, Rating = 3, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 5, UserID = 3, Rating = 2, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 6, UserID = 3, Rating = 1, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 7, UserID = 4, Rating = 3, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 8, UserID = 4, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 9, UserID = 5, Rating = 5, Review = "Good", isLike = true },
+
+            };
+            foreach (var list in ListProductReview)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " " + "UserId: " + list.UserID + " " + "Rating: " + list.Rating + " "
+                    + "Review: " + list.Review + " " + list.isLike + " ");
+            }
+
+            Console.WriteLine("-------------------------------------------------------------------");
+
+            Management management = new Management();
+
+            management.TopRecordsList(ListProductReview);
+
+            management.SelectedRecords(ListProductReview);
+            management.RetrieveCountOfRecords(ListProductReview);
+        }
+    }
+}
